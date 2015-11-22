90524416297880ca8e7b5697191104953cb1b0c3
Javi520 <javi520@live.com>
2015-11-22 14:22:40 +0100

    Revert "iwnbmoc"
    This reverts commit 2a0bd5f768425605739f1bf6a4ab1b47b83ab1e0.

--- st_tree (copy).ml
+++
@@ -1,8 +0,0 @@
-type 'a st_tree =
-     S of 'a
-   | C of 'a * 'a st_tree * 'a st_tree;;
-exception Ramas;;
-let single e = S e;;
-let comp r (i,d) = C (r,i,d);;
-let raiz = function S r | C (r,_,_) -> r;;
-let ramas = function C (_,i,d) -> (i,d) | _ -> raise Ramas;;
